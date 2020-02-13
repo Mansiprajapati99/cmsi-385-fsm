91acc9088be188878946de23099975aaf0e6ec55
Mansiprajapati99 <60244762+Mansiprajapati99@users.noreply.github.com>
2020-02-12 19:28:16 -0800

    Update DeterministicFiniteStateMachine.js

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -3,7 +3,10 @@ export default class DeterministicFiniteStateMachine {
   /**
    */
   constructor({ transitions, startState, acceptStates }) {
-    throw 'IMPLEMENT constructor(description)';
+    
+    this.transitions=transitions;
+    this.startState=startState;
+    this.acceptStates=acceptStates;
   }
 
   /**
@@ -11,11 +14,37 @@ export default class DeterministicFiniteStateMachine {
    * @returns a string state name
    */
   transition(state, symbol) {
-    throw 'IMPLEMENT transition(state, symbol)';
+    return this._transitions[state][symbol];
+  }
+ 
+  isAcceptState(state)
+  {
+    return this.acceptStates.includes(state);
   }
 
+
+  accept(string,state=this.startState)
+  {
+    const nextState = this.transition(state,string.charAt((0)));
+    return 
+
+
+  }
+
+  /*
   accepts(string, state = this._startState) {
-    throw 'IMPLEMENT FiniteStateMachine.accepts';
+    if(string.length === 0)
+    {
+        return this.isAcceptState(state);
+
+    }else{
+
+        const nextState = this.transition(state,string.charAt(0));
+        return this.accepts(string.substr(1),nextState);
+    }
+*/
+
+    
   }
 
 }
