29bb8ec502ac7b30b73f00b0f5b6dec0f6aeb5de
Mansiprajapati99 <60244762+Mansiprajapati99@users.noreply.github.com>
2020-02-14 14:39:06 -0800

    Update DeterministicFiniteStateMachine.js

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -3,10 +3,9 @@ export default class DeterministicFiniteStateMachine {
   /**
    */
   constructor({ transitions, startState, acceptStates }) {
-    
-    this.transitions=transitions;
-    this.startState=startState;
-    this.acceptStates=acceptStates;
+    this._transitions = transitions;
+    this._startState = startState;
+    this._acceptStates = acceptStates;
   }
 
   /**
@@ -16,34 +15,28 @@ export default class DeterministicFiniteStateMachine {
   transition(state, symbol) {
     return this._transitions[state][symbol];
   }
- 
   isAcceptState(state)
   {
-    return this.acceptStates.includes(state);
+    return this._acceptStates.includes(state);
   }
-
-
-  accepts(string,state=this.startState)
-  {
-    const nextState = this.transition(state,string.charAt((0)));
-    return (string.length === 0) ? this.isAcceptState(state):
-                                    this.accepts(string.substr(1),nextState);
-
-
-  }
-
   /*
   accepts(string, state = this._startState) {
-    if(string.length === 0)
-    {
-        return this.isAcceptState(state);
-    }else{
-        const nextState = this.transition(state,string.charAt(0));
-        return this.accepts(string.substr(1),nextState);
-    }
-*/
+    const nextState = this.transition(state, string.charAt(0));
+    return (string.length === 0) ? this.isAcceptState(state):
+                                   this.accepts(string.substr(1),nextState);
+  } */
 
-    
+  accepts(string, state= this._startState)
+  {
+     var nextState = this.transition(state,string.charAt(0));
+     if(string.length === 0)
+     {
+       this.isAcceptState(state);
+     }else{
+       this.accepts(string.substr(1),nextState);
+     }
+     
+
   }
 
 }
